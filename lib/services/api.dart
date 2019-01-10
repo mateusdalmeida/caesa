b68cf85935955964860fd920e29fc8689d9f0a58
ANDERSON DOS SANTOS GUERRA <anderson.guerra@dinfo.br>
2019-01-10 15:59:38 -0300

    Uso de future.delayed implementado para simular requisicoes à api

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -8,33 +8,51 @@ import 'package:caesa/models/cliente.dart';
 
 //const baseUrl = 'http://192.168.0.1/';
 
-List<Cliente> getCliente(){
+List<Cliente> getCliente() {
   List<Cliente> clientes = new List();
-  clientes.add(Cliente(inscricao: 25, nome: "Anderson", numeroPorta: "Teste", categoria: 30, idLogradouro: 12, cidade: 32));
+  clientes.add(Cliente(
+      inscricao: 25,
+      nome: "Anderson",
+      numeroPorta: "Teste",
+      categoria: 30,
+      idLogradouro: 12,
+      cidade: 32));
   return clientes;
 }
 
-List<Debito> getDebito(){
+List<Debito> getDebito() {
   List<Debito> debitos = new List();
-  debitos.add(Debito(refFaturamento: 12, dataVencimento: 203, valorTotal: 13.5, volume: 26.9, origem: 95));
-  debitos.add(Debito(refFaturamento: 12, dataVencimento: 203, valorTotal: 13.5, volume: 26.9, origem: 95));
+  debitos.add(Debito(
+      refFaturamento: 12,
+      dataVencimento: 203,
+      valorTotal: 13.5,
+      volume: 26.9,
+      origem: 95));
+  debitos.add(Debito(
+      refFaturamento: 12,
+      dataVencimento: 203,
+      valorTotal: 13.5,
+      volume: 26.9,
+      origem: 95));
   return debitos;
 }
-
 
 Future<List<Cliente>> getClientes(int inscricao) async {
   //final response = await http.get("$baseUrl/api/cliente/matricula/dados/$inscricao");
   List<Cliente> clientes = new List();
 
-  clientes.add(Cliente(inscricao: 25, nome: "Anderson", numeroPorta: "305", categoria: 02, idLogradouro: 12, cidade: 201));
+  clientes.add(Cliente(
+      inscricao: 25,
+      nome: "Anderson",
+      numeroPorta: "305",
+      categoria: 02,
+      idLogradouro: 12,
+      cidade: 201));
 
-  dynamic test = await Future.delayed(Duration(seconds: 15), (){
-    return clientes;
-  });
-
-  
-
-  return test;
+  print("antes do await");
+  await Future.delayed(Duration(seconds: 5));
+  print("apos await");
+  return clientes;
 
   // if (response.statusCode == 200) {
   //   var responseJson = json.decode(response.body);
--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:caesa/services/api.dart' as api;
 
 class Login extends StatefulWidget {
   @override
   _LoginState createState() => _LoginState();
 }
 
-class _LoginState extends State<Login> with SingleTickerProviderStateMixin  {
+class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
   TabController _tabController;
   int controller = 0;
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
     _tabController =
         new TabController(vsync: this, length: 2, initialIndex: controller);
+
+    // Chama a tela de "aguardar"
+    api.getClientes(123).then((result) {
+      // repara que aqui eu não utilizei o await, porque a função initState não pode ser async
+      // por isso usei .then (parecido com o js da vida)
+      print(result[0].nome);
+      setState(() {
+        // Após o delay, dispensa a tela de "aguardar"
+        _isLoading = false;
+      });
+    });
   }
 
   @override
@@ -23,121 +36,112 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin  {
         title: Text("CAESA - 2ª via"),
         centerTitle: true,
       ),
-      body: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(bottom: 15),
-                child: Text( 
-                  "Escolha a forma de acesso",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                  textAlign: TextAlign.justify,
-                ) 
-              ),
-              Row(
+      body: _isLoading == true
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
                 children: <Widget>[
-                  Expanded(
-                    child: GestureDetector(
-                      child: Container(
-                        height: 40,
-                        child: Center(
-                          child: Text(
-                            "MATRICULA",
-                            style: TextStyle(
-                              color: Colors.blue
-                            ),
-                          ),
+                  Padding(
+                      padding: EdgeInsets.only(bottom: 15),
+                      child: Text(
+                        "Escolha a forma de acesso",
+                        style: TextStyle(
+                          fontSize: 20,
                         ),
-                        decoration: BoxDecoration(
-                          border: Border(
-                            bottom: BorderSide(
-                              color: controller== 0 ? Colors.grey[200] : Colors.blue,
-                              width: 2
-                            )
-                          )
-                        ),   
-                      ),
-                      onTap: (){
-                        _tabController.animateTo(0);
-                        setState(() {
-                          controller = 1;                      
-                        });
-                      },
-                    ),
-                  ),  
-                  Expanded(
-                    child: GestureDetector(
-                      child: Container(
-                        height: 40,
-                        child: Center(
-                          child: Text(
-                            "CPF",
-                            style: TextStyle(
-                              color: Colors.blue
+                        textAlign: TextAlign.justify,
+                      )),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: GestureDetector(
+                          child: Container(
+                            height: 40,
+                            child: Center(
+                              child: Text(
+                                "MATRICULA",
+                                style: TextStyle(color: Colors.blue),
+                              ),
                             ),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(
+                                        color: controller == 0
+                                            ? Colors.grey[200]
+                                            : Colors.blue,
+                                        width: 2))),
                           ),
+                          onTap: () {
+                            _tabController.animateTo(0);
+                            setState(() {
+                              controller = 1;
+                            });
+                          },
                         ),
-                        decoration: BoxDecoration(
-                          border: Border(
-                            bottom: BorderSide(
-                              color: controller == 0 ? Colors.blue : Colors.grey[200],
-                              width: 2
-                            )
-                          )
-                        ),   
                       ),
-                      onTap: (){
-                        _tabController.animateTo(1);
-                        setState(() {
-                          controller = 0;                      
-                        });
-                      },
+                      Expanded(
+                        child: GestureDetector(
+                          child: Container(
+                            height: 40,
+                            child: Center(
+                              child: Text(
+                                "CPF",
+                                style: TextStyle(color: Colors.blue),
+                              ),
+                            ),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(
+                                        color: controller == 0
+                                            ? Colors.blue
+                                            : Colors.grey[200],
+                                        width: 2))),
+                          ),
+                          onTap: () {
+                            _tabController.animateTo(1);
+                            setState(() {
+                              controller = 0;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                      child: TabBarView(
+                    controller: _tabController,
+                    // Restrict scroll by user
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: <Widget>[
+                      Form(
+                        child: TextFormField(
+                          autofocus: true,
+                          decoration: InputDecoration(
+                              hintText: "Insira sua Matricula aqui",
+                              labelText: "Matricula"),
+                        ),
+                      ),
+                      Form(
+                        child: TextFormField(
+                          autofocus: true,
+                          decoration: InputDecoration(
+                              hintText: "Insira seu CPF Aqui",
+                              labelText: "CPF"),
+                        ),
+                      ),
+                    ],
+                  )),
+                  Expanded(
+                    child: RaisedButton(
+                      child: Text("Entrar"),
+                      onPressed: () {},
                     ),
-                  ),  
+                  )
                 ],
               ),
-              Expanded(
-                child: TabBarView(
-                  controller: _tabController,
-                  // Restrict scroll by user
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: <Widget>[
-                    Form(
-                      child: TextFormField(
-                        autofocus: true,
-                        decoration: InputDecoration(
-                          hintText: "Insira sua Matricula aqui",
-                          labelText: "Matricula"
-
-                        ),
-                      ),
-                    ),
-                    Form(
-                      child: TextFormField(
-                        autofocus: true,
-                        decoration: InputDecoration(
-                          hintText: "Insira seu CPF Aqui",
-                          labelText: "CPF"
-
-                        ),
-                      ),
-                    ),
-                  ],
-                )
-              ),
-              Expanded(
-                child: RaisedButton(
-                  child: Text("Entrar"),
-                  onPressed: (){},
-                ),
-              )
-            ],
-          ),
-        ),
-      
+            ),
     );
   }
 }
