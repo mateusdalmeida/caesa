6e2eb72b84b523f5f24ef05d659b1e822310016e
Mateus <mateusdalmeida0@gmail.com>
2019-01-27 16:10:37 -0300

    Faturas

--- lib/ui/faturas.dart
+++ lib/ui/faturas.dart
@@ -1,46 +1,44 @@
 import 'package:flutter/material.dart';
 
 //mport 'package:caesa/services/api.dart' as api;
-import 'package:caesa/models/cliente.dart';
+import 'package:caesa/models/debito.dart';
 
-class Matriculas extends StatefulWidget {
-
-  List<Cliente> matriculas;
+class Faturas extends StatefulWidget {
+  List<Debito> debitos;
 
   // aqui tu obriga o construtor a ter o parametro matricula, tudo okay
-  Matriculas(this.matriculas);
+  Faturas(this.debitos);
 
-  _MatriculasState createState() => _MatriculasState();
+  _FaturasState createState() => _FaturasState();
 }
 
-class _MatriculasState extends State<Matriculas> {
-
+class _FaturasState extends State<Faturas> {
   // essa é uma classe diferente da de cima, então é necessário declarar aqui também
-  List<Cliente> matriculas;
+  List<Debito> debitos;
 
   @override
-    void initState() {
-      // TODO: implement initState
-      super.initState();
-      // na inicialização do widget eu pego o valor da matricula da classe de cima com widget.matriculas
-      // o widget. é usado pra pegar os valores da classe que extende statefulwidget
-      matriculas = widget.matriculas;
-    }
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // na inicialização do widget eu pego o valor da matricula da classe de cima com widget.matriculas
+    // o widget. é usado pra pegar os valores da classe que extende statefulwidget
+    debitos = widget.debitos;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("CAESA - Matriculas"),
+        title: Text("CAESA - Faturas"),
         centerTitle: true,
       ),
       // nao faz sentido ter um futurebuilder aqui, a requisição é a mesma utilizada na tela anterior
-      body: _createMatriculasList(context, matriculas),
+      body: _createDebitosList(context, debitos),
     );
   }
 }
 
-Widget _createMatriculasList(BuildContext context, List<Cliente> clientes) {
+Widget _createDebitosList(BuildContext context, List<Debito> debitos) {
   return SingleChildScrollView(
       child: Column(
     crossAxisAlignment: CrossAxisAlignment.start,
@@ -48,7 +46,7 @@ Widget _createMatriculasList(BuildContext context, List<Cliente> clientes) {
       Padding(
         padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
         child: Text(
-          "Bem vindo(a) ${clientes[0].nome}\nEssas são as matriculas em seu nome",
+          "Suas Faturas\nMatricula:",
           textAlign: TextAlign.left,
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
@@ -56,13 +54,13 @@ Widget _createMatriculasList(BuildContext context, List<Cliente> clientes) {
       ListView.separated(
         shrinkWrap: true,
         physics: ClampingScrollPhysics(),
-        itemCount: clientes.length,
+        itemCount: debitos.length,
         itemBuilder: (context, index) {
           return GestureDetector(
-              child: ListTile(
-            title: Text("Matricula: ${clientes[index].inscricao}"),
+            child: ListTile(
+            title: Text("Matricula: ${debitos[index].refFaturamento}"),
             subtitle: Text(
-                "${clientes[index].idLogradouro}\n${clientes[index].categoria}"),
+                "Vencimento: ${debitos[index].dataVencimento}\nR\$ ${debitos[index].valorTotal}"),
             isThreeLine: true,
           ));
         },
